f9408336cf6792d0fcc64c51ee61087652ca7e89
iltenma <36898842+iltenma@users.noreply.github.com>
2018-06-08 15:18:16 +0200

    Add files via upload

---
+++ Ultrasonido_ARD.ino
@@ -0,0 +1,39 @@
+const int trigpin = 9;
+const int echopin = 4;
+long duration;
+int distance;
+byte valDig = 0;
+int valAnalogico = 0;
+int hayPersona = 0;
+
+void setup() {
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+  duration = pulseIn(echopin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  if (hayPersona == 0) {
+    if (distance < 80) {
+        hayPersona = 1;
+      Serial.write(hayPersona);
+      //Serial.println(hayPersona);
+    }
+  }
+  
+  if (hayPersona == 1) {
+    if (distance >= 80) {
+       hayPersona = 0;
+     Serial.write(hayPersona);
+     //Serial.println(hayPersona);
+    }
+     delay(500);
+  }
+     // Serial.println(hayPersona);
+}
